c435ba27ebabfea6f61482b7d3cc6dfa566b3162
totitos <anton196503@gmail.com>
2025-04-08 17:12:35 +0300

    бот - начало

--- main.py
+++ main.py
@@ -1,59 +1,20 @@
-import numpy as np
-import yadisk
-import pandas as pd
-from io import BytesIO
+import asyncio
+import logging
 
-# Настройки
-OAUTH_TOKEN = 'y0__xD5u5uzBhiP2jYgqtfH3BJPdoNuBfWhA76z5-DYzuauTa1Ngw'
-FILE_PATH_IVT = 'Загрузки/Текущая успеваемость 1 курс/ИВТ журнал оценок.xlsx'  # Например, 'Рабочие/данные.xlsx'
-FILE_PATH_IT = 'Загрузки/Текущая успеваемость 1 курс/ИТ журнал оценок.xlsx'
-FILE_PATH_PIE = 'Загрузки/Текущая успеваемость 1 курс/ПИЭжурнал оценок.xlsx'
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
-y = yadisk.YaDisk(token=OAUTH_TOKEN)
+import config
+from yd.yd_api import get_by_stud_id, FILE_PATH_PIE, FILE_PATH_IVT, FILE_PATH_IT
 
-
-def read_excel_from_yadisk(file_path):
-    # Скачиваем файл в память
-    file_bytes = BytesIO()
-    y.download(file_path, file_bytes)
-    file_bytes.seek(0)
-
-    # Читаем Excel файл
-    return pd.ExcelFile(file_bytes)
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
 
 
-# Использование
-
-def get_df(file_path):
-    try:
-        excel_data = read_excel_from_yadisk(file_path)
-        # Чтение всех листов
-        # for sheet_name in excel_data.sheet_names:
-        #     df = excel_data.parse(sheet_name, skiprows=2)
-        #     print(f"Лист: {sheet_name}")
-        #     # print(df.iat[1, 1])
-        #     print(df.head())
-
-        all_sheets = excel_data.parse(skiprows=2, sheet_name=None)
-        combined_df = pd.concat(all_sheets.values(), ignore_index=True)
-        combined_df = combined_df.drop(columns=["№"])
-        combined_df = combined_df.dropna(subset=["Студенч. номер"])
-        combined_df["Студенч. номер"] = combined_df["Студенч. номер"].apply(lambda x: f"{str(x)[:7]}")
-
-        return combined_df
-    except Exception as e:
-        return f"Ошибка: {e}"
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    await dp.start_polling(bot)
 
 
-# print(get_df(FILE_PATH_PIE))
-
-
-def get_by_stud_id(stud_id, file_path):
-    df = get_df(file_path)
-    if df["Студенч. номер"].isin([stud_id]).any():
-        row = df.loc[df["Студенч. номер"] == stud_id].iloc[0]
-        return row
-    return "Нет совпадений"
-
-
-print(get_by_stud_id("1036399", FILE_PATH_PIE))
+if __name__ == "__main__":
+    asyncio.run(main())
